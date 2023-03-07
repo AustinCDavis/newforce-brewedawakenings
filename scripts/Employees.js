f11c6790e8d71b9e4637f8cbbcdaa33567eefb82
Austin Davis <davis.austin1.1.c@gmail.com>
2023-03-07 15:11:50 -0500

    Completed Top SalesPerson

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,39 @@
-import { getEmployees } from "./database.js"
-
+import { getEmployees, getOrders } from "./database.js"
 const employees = getEmployees()
+const orders = getOrders()
+
+const filterOrders = (singleOrder) => {
+    let employeeSales = 0;
+
+    for (const order of orders) {
+        if (order.employeeId === singleOrder.id) {
+            employeeSales += 1
+        }
+        
+    }
+    return employeeSales
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.id.startsWith("employee")) {
+
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const productsSold = filterOrders(employee) 
+
+                    window.alert(`${employee.name} sold ${productsSold} products`)
+                }
+            }
+        }
+    }
+)
 
 export const Employees = () => {
     let html = "<ul>"
